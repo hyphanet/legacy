3b74666a1c0abcaad6b89aaf3fe2bda672397749
Florent Daignière <nextgens@freenetproject.org>
2006-05-17 11:37:04 +0000

    legacy: update the update script
    git-svn-id: file:///home/toad/git-migration/temprepository/branches/legacy/stable@8738 67a373e5-eb02-0410-a15c-ee090a768436

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 cp freenet.jar freenet.jar.old
 cp seednodes.ref seednodes.ref.old
-wget http://freenetproject.org/snapshots/freenet-latest.jar -O freenet-latest.jar
+wget http://downloads.freenetproject.org/freenet-latest.jar -O freenet-latest.jar
 if test -s freenet-latest.jar; then
     mv -f freenet-latest.jar freenet.jar
 fi
-wget http://freenetproject.org/snapshots/seednodes.ref.bz2 -O new-seednodes.ref.bz2
+wget http://downloads.freenetproject.org/seednodes.ref.bz2 -O new-seednodes.ref.bz2
 if test -s new-seednodes.ref.bz2; then
     bzcat new-seednodes.ref.bz2 >seednodes.ref
     rm -f new-seednodes.ref.bz2
 fi
-wget -N http://freenetproject.org/snapshots/freenet-ext.jar
+wget -N http://downloads.freenetproject.org/freenet-ext.jar
 
 touch -t "197001011200" seednodes.ref || touch -d "1/1/1970" seednodes.ref
 # so we don't reseed unless necessary
