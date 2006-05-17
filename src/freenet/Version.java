3cb893c033a999693cf0af084293bbd2ba9dd1ec
Matthew Toseland <toad@amphibian.dyndns.org>
2006-05-17 11:24:55 +0000

    5107: Fix initial startup failure when libjbigi cannot be loaded.
    git-svn-id: file:///home/toad/git-migration/temprepository/branches/legacy/stable@8734 67a373e5-eb02-0410-a15c-ee090a768436

--- src/freenet/Version.java
+++ src/freenet/Version.java
@@ -20,7 +20,7 @@ public abstract class Version {
 	public static String protocolVersion = "STABLE-1.51";
 
 	/** The build number of the current revision */
-	public static final int buildNumber = 5106;
+	public static final int buildNumber = 5107;
 
 	/** Oldest build of Fred we will talk to */
 	public static final int lastGoodBuild = 5099;
